5307693d86ff2e3710de41edda4c10f66e5be000
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-28 17:28:37 +0300

    Game logic: highlight the cells available for movement

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './Board.module.scss';
 import Cell from '../Cell/Cell';
 import { Board as BoardClass } from 'src/models/Board';
+import { Cell as CellClass } from 'src/models/Cell';
 
 interface BoardProps {
   board: BoardClass;
@@ -9,12 +10,42 @@ interface BoardProps {
 }
 
 const Board: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<CellClass | null>(null);
+
+  useEffect(() => {
+    highlightCells();
+  }, [selectedCell]);
+
+  function clickCell(cell: CellClass) {
+    if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell);
+      setSelectedCell(null);
+    } else {
+      setSelectedCell(cell);
+    }
+  }
+
+  function highlightCells() {
+    board.highlightCells(selectedCell);
+    updateBoard();
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard();
+    setBoard(newBoard);
+  }
+
   return (
     <div className={styles.board}>
       {board.cells.map((row, index) => (
         <React.Fragment key={index}>
           {row.map((cell) => (
-            <Cell cell={cell} key={cell.id} />
+            <Cell
+              clickCell={clickCell}
+              cell={cell}
+              key={cell.id}
+              selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+            />
           ))}
         </React.Fragment>
       ))}
--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -4,11 +4,20 @@ import { Cell as CellClass } from 'src/models/Cell';
 
 interface CellProps {
   cell: CellClass;
+  selected: boolean;
+  clickCell: (cell: CellClass) => void;
 }
 
-const Cell: FC<CellProps> = ({ cell }) => {
+const Cell: FC<CellProps> = ({ cell, selected, clickCell }) => {
+  console.log(selected);
   return (
-    <div className={[styles.cell, styles[cell.color]].join(' ')}>
+    <div
+      className={[styles.cell, styles[cell.color], selected ? styles.selected : ''].join(' ')}
+      onClick={() => clickCell(cell)}
+      // Delete this!
+      style={{ background: cell.available && cell.figure ? 'green' : '' }}
+    >
+      {cell.available && !cell.figure && <div className={styles.available}></div>}
       {cell.figure?.logo && <img src={cell.figure.logo} alt='' />}
     </div>
   );
