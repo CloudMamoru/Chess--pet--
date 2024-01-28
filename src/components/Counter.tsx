56c6788588cf23cae4db72d472905f4f31e4e142
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2024-01-28 13:31:30 +0300

    Webpack: Connect css modules

---
+++ src/components/Counter.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Counter.module.scss';
+
+const Counter = () => {
+  return <div className={styles.Test}>Counter</div>;
+};
+
+export default Counter;
